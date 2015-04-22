bbbbdcf9bc94e40640ae278e0d31f4140e2a7805
David Karchmer <dkarchmer@ampervue.com>
2015-04-21 23:51:46 -0700

    Example to compile code to change a color picture into a Grey scale one

---
+++ example/app/opencvtest.cpp
@@ -0,0 +1,25 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace cv;
+
+int main( int argc, char** argv )
+{
+ char* imageName = argv[1];
+
+ Mat image;
+ image = imread( imageName, 1 );
+
+ if( argc != 2 || !image.data )
+ {
+   printf( " No image data \n " );
+   return -1;
+ }
+
+ Mat gray_image;
+ cvtColor( image, gray_image, CV_BGR2GRAY );
+
+ imwrite( "Gray_Image.jpg", gray_image );
+
+ return 0;
+}
